2918e518ceb55bc113fe0da4c1308651d8b09016
APNPAPNU <jcpgraphicdesign@gmail.com>
2018-09-06 22:43:23 -0400

    Update api.js

--- _includes/js/api.js
+++ _includes/js/api.js
@@ -1,4 +1,4 @@
 apiKey = "47b810e692d64237911c2cbe0d433cfe", // production
-// apiKey = "6987280b74b24575a4e805277bb5baa6", // local
+// apiKey = "47b810e692d64237911c2cbe0d433cfe", // local
 roiGroupId = "2974952",
 exaltedGroupId = "3085051"

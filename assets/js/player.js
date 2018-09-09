5b43614940e6da373cee8ebd9a1372ff3c3c0199
APNPAPNU <jcpgraphicdesign@gmail.com>
2018-09-09 15:01:47 -0400

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -5,7 +5,6 @@ $(function() {
   var
     {% include js/ranks.js %},
     bungieId = checkParams('bungieId'),
-    exalted = checkParams('exalted'),
     destinyId = checkParams('destinyId'),
     joined = checkParams('joined'),
     rank = checkParams('rank'),
@@ -33,7 +32,7 @@ $(function() {
 
   if (bungieId && destinyId && joined && rank) {
     $.ajax({
-      url: "https://www.bungie.net/Platform/Destiny2/4/Account/" + destinyId + "/Character/0/Stats/",
+      url: "https://www.bungie.net/Platform/Destiny2/2/Account/" + destinyId + "/Character/0/Stats/",
       headers: {
         "X-API-Key": apiKey
       },

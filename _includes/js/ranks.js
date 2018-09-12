53882b71b134b52569a287f56d6a440cb2269def
APNPAPNU <jcpgraphicdesign@gmail.com>
2018-09-12 11:44:25 -0400

    Create ranks.js

---
+++ _includes/js/ranks.js
@@ -0,0 +1,36 @@
+honored = [
+	'agbockus',
+	'Batman',
+	'brandino',
+	'CyberDemon',
+	'Drazah',
+	'DJCHRIS679',
+	'Dyxlesic',
+	'Jazzy',
+  'JoeCorbeux',
+	'JPo203',
+	'Luna',
+	'Pike',
+	'Renzo',
+	'Riperino',
+	'RulinPoleCat',
+  'Rumps',
+	'SpaceCorgo',
+	'TeaKettle',
+	'TehMadBear',
+	'Tratiax',
+	'Vipper',
+	'Wednesday',
+	'ZeroC00L',
+],
+revered = [
+	'BONEY',
+	'Severik',
+	'Sore',
+],
+exaltedGeneral = [
+	'rickypettit12',
+],
+founder = [
+	'Canis'
+]

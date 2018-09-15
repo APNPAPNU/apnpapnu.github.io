7340c13462ce73945d30bb75ed14beda8ea71928
APNPAPNU <jcpgraphicdesign@gmail.com>
2018-09-14 20:08:30 -0400

    Create roster.js

---
+++ assets/js/roster.js
@@ -0,0 +1,10 @@
+$.ajax({
+  url: "https://www.bungie.net/platform/GroupV2/699392/Members/",
+  headers: {
+    "X-API-Key": "47b810e692d64237911c2cbe0d433cfe"
+  }
+}).done(function(json) {
+  var members = json.Response.results;
+  console.log('Member list:', members);
+  listMembers(members);
+});

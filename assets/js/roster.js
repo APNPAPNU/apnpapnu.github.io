5c859a8d48695b4b305be8dc978466a06fe3e30b
APNPAPNU <jcpgraphicdesign@gmail.com>
2018-09-14 20:19:44 -0400

    Update roster.js

--- assets/js/roster.js
+++ assets/js/roster.js
@@ -5,8 +5,7 @@
   headers: {
     "X-API-Key": "47b810e692d64237911c2cbe0d433cfe"
   }
-}).done(function(json) {
-  var members = json.Response.results;
+}).done(function(json) 
   console.log('Member list:', members);
   listMembers(members);
 });

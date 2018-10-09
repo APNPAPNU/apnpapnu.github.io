a3eaa666fffa9d70edde6519e8156f640f3bd39b
APNPAPNU <jcpgraphicdesign@gmail.com>
2018-10-08 20:40:54 -0400

    Update and rename gambit to gambit.js

--- gambit
+++
@@ -1,5 +0,0 @@
-$.ajax({
-      url: "https://www.bungie.net/Platform/Destiny2/2/Account/" + destinyId + "/Character/0/Stats/",
-      headers: {
-        "X-API-Key": apiKey
-      },
---
+++ gambit.js
@@ -0,0 +1,38 @@
+---
+---
+$(function() {
+
+  var
+  {% include js/api.js %},
+    bungieId = checkParams('bungieId'),
+    destinyId = checkParams('destinyId'),
+    joined = checkParams('joined'),
+    checkName = function(name, list) {
+
+      var m = false; // flag
+      console.log('Checking list for ' + name + '...');
+
+      // loop through clan usernames and check for a match
+      $.each(list, function(i) {
+        // make case insensitve
+        if (name.toLowerCase() === list[i].toLowerCase()) {
+          console.log('Confirmed: ' + list[i]);
+          m = true;
+        }
+      });
+
+      if (m) {
+        return true;
+      } else {
+        return false;
+      }
+
+    };
+
+  if (bungieId && destinyId && joined) {
+    $.ajax({
+      url: "https://www.bungie.net/Platform/Destiny2/2/Account/" + destinyId + "/Character/0/Stats/",
+      headers: {
+        "X-API-Key": apiKey
+      },
+      success: function(data)

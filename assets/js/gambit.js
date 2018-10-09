6547170992a8e39dfc898b5d84984b53d85309f8
APNPAPNU <jcpgraphicdesign@gmail.com>
2018-10-09 10:30:22 -0400

    Update gambit.js

--- assets/js/gambit.js
+++ assets/js/gambit.js
@@ -4,32 +4,7 @@ $(function() {
 
   var
   {% include js/api.js %},
-    bungieId = checkParams('bungieId'),
-    destinyId = checkParams('destinyId'),
-    joined = checkParams('joined'),
-    checkName = function(name, list) {
-
-      var m = false; // flag
-      console.log('Checking list for ' + name + '...');
-
-      // loop through clan usernames and check for a match
-      $.each(list, function(i) {
-        // make case insensitve
-        if (name.toLowerCase() === list[i].toLowerCase()) {
-          console.log('Confirmed: ' + list[i]);
-          m = true;
-        }
-      });
-
-      if (m) {
-        return true;
-      } else {
-        return false;
-      }
-
-    };
-
-  if (bungieId && destinyId && joined) {
+  
     $.ajax({
       url: "https://www.bungie.net/Platform/Destiny2/2/Account/4611686018429000034/Character/0/Stats/?groups=0,0&modes=63&periodType=0",
       headers: {

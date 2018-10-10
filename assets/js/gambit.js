169a4afdf6a024208d342575c81a5fd80eddd9e2
APNPAPNU <jcpgraphicdesign@gmail.com>
2018-10-09 21:54:50 -0400

    Update gambit.js

--- assets/js/gambit.js
+++ assets/js/gambit.js
@@ -1,5 +1,5 @@
 $.ajax({
-  url: "https://www.bungie.net/Platform/Destiny2/2/Account/4611686018429000034/Character/0/Stats/?groups=0,0&modes=63&periodType=0"",
+  url: "https://www.bungie.net/Platform/Destiny2/2/Account/4611686018429000034/Character/0/Stats/?groups=0,0&modes=63&periodType=0",
   headers: {
     "X-API-Key": apiKey
   }

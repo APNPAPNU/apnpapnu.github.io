5f314d9fe7b5fbb5dfff03d02f610262e75ced0d
j <jcpgraphicdesign@gmail.com>
2018-10-21 20:37:29 -0400

    gotnrid of crap

--- side/ckkbigakdbjgjaednmblhhmflefmehop/background.js
+++
@@ -1,8 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('index.html', {
-        'bounds': {
-            'width': Math.round(window.screen.availWidth*0.95),
-            'height': Math.round(window.screen.availHeight*0.95)
-        }
-    });
-});

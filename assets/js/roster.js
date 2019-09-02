5b0c5132fe7a3a9ca3cbc7f8d13294241dfdada9
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-09-02 11:31:39 -0400

    Update roster.js

--- assets/js/roster.js
+++ assets/js/roster.js
@@ -117,7 +117,8 @@ function listMembers(rsp) {
 var
 CharacterId = response.data.characters.characterbase.CharacterId;
 	    $('#CHaracter-Id').text(CharacterId);
-        .html(
+	    html
+	    .html(
           '<div class="j-col j-col-1 member-icon"><img src="https://bungie.net/' + icon + '"></div>' +
           '<div class="j-col j-col-3 member-name"><h3>' + name + '</h3></div>' +
           '<div class="j-col j-col-3 member-joined" data-label="Joined">' + joined.replace(/-/g, '/') + '</div>' +

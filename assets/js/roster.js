a4c40915f318b3f5c8d0d49d7417c1222d2f0757
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-09-02 23:50:44 -0400

    Update roster.js

--- assets/js/roster.js
+++ assets/js/roster.js
@@ -110,7 +110,6 @@ function listMembers(rsp) {
 		url: "https://www.bungie.net/Platform/Destiny/2/Account/"+ destinyId +"/",
 		headers: {
 			"X-API-Key":"47b810e692d64237911c2cbe0d433cfe"
-		}
 	})
 	.success(function(Response) {
 		

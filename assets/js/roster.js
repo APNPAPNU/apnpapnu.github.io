559f959ace5cab0dcf1aff2fe0c87c6395538fee
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-09-03 18:30:57 -0400

    Update roster.js

--- assets/js/roster.js
+++ assets/js/roster.js
@@ -127,8 +127,8 @@ $(function() {
       error: function(data) {
 				alert('Uh oh, failed to load player stats! Looks like Bungie\'s doing server maintenance or having problems. Please check back again soon!');
         console.log('Error loading player stats:', data);
-		  '<button onclick="window.location='https://braytech.org/2/'+ destinyId +'/'+ fstats +'/legend';">Visit Page Now</button>'   }
-	    
+		  '<div class="j-col j-col-4 member-button"><a href="https://braytech.org/2/'+ destinyId +'/'+ fstats +'/legend">In Depth Stats</a>' + '</a></div>'    
+      }
 	  });
 });
       member

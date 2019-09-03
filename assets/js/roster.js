1a0ac801cfc97e954cfd13fdc8d0dfca98ec4535
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-09-03 19:11:13 -0400

    Update roster.js

--- assets/js/roster.js
+++ assets/js/roster.js
@@ -146,8 +146,7 @@ $(function() {
         '<div class="j-col j-col-3 member-joined" data-label="Joined">' + joined.replace(/-/g, '/') + '</div>' +
         '<div class="j-col j-col-3 member-status" data-label="Status"><span class="member-online" id="status-' + memberId + '">' + online + '</span></div>' +
         '<div class="j-col j-col-3 member-button"><a class="button outline gold full-width">' + 'View Stats' + '</a></div>'+
-	       '<div class="j-col j-col-3 members-button"><a href="https://braytech.org/2/'+ destinyId +'/'+ CharacterId +'/legend">In Depth Stats</a>' + '</a></div>'    
-   
+	  
 	 )
 	 });
 	 append

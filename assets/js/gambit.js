c8ac4bce2ff7beb6c6ff9889574fab3addd8482b
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-09-04 17:03:32 -0400

    Update gambit.js

--- assets/js/gambit.js
+++ assets/js/gambit.js
@@ -8,6 +8,7 @@ $.ajax({
 });
 $(function() {
 
+  
     $.ajax({
       url: "https://www.bungie.net/Platform/Destiny/2/Account/4611686018429000034/",
       headers: {
@@ -21,11 +22,10 @@ $(function() {
 
 	        // Populate stats
           // pvp
-      
-	        $('#player-f-stats').text(fstats);
-	
+      html
+          .html( '<div class="j-col j-col-3 member-button"> + <a href="https://braytech.org/2/'+ 4611686018429000034 +'/'+ fstats +'/legend">In Depth Stats</a>' + '</a></div>'
+          )
 	    }
-              .html( '<div class="j-col j-col-3 member-button"> + <a href="https://braytech.org/2/'+ 4611686018429000034 +'/'+ fstats +'/legend">In Depth Stats</a>' + '</a></div>'
-      )
+              
    });
 	});

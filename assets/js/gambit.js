809d55cfe3a84871f9cf54b65c270d5e938c3490
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-09-04 16:00:04 -0400

    Update gambit.js

--- assets/js/gambit.js
+++ assets/js/gambit.js
@@ -24,10 +24,11 @@ $(function() {
           // pvp
 	   
 	        $('#player-f-stats').text(fstats);
-	        
+	        }
+	      .html(
           		'<div class="j-col j-col-3 member-button"> + <a href="https://braytech.org/2/'+ 4611686018429000034 +'/'+ fstats +'/legend">In Depth Stats</a>' + '</a></div>';
-			
+			)
 
-      },
+      
     });
 	});

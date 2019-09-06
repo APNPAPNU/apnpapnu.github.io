0aa30406513691eee7cfe78bd771f6c11c5e19de
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-09-06 17:40:40 -0400

    Update gambit.js

--- assets/js/gambit.js
+++ assets/js/gambit.js
@@ -4,22 +4,19 @@ $.ajax({
       "X-API-Key": "47b810e692d64237911c2cbe0d433cfe"
     }
   }).done(function(json) {
-  
+
   });
   $(function() {
-  
-    
       $.ajax({
         url: "https://www.bungie.net/Platform/Destiny/2/Account/4611686018429000034/",
         headers: {
           "X-API-Key": apiKey
         },
         success: function(data) {
-  var fstats = data.Response.data.characters[0].characterBase.characterId;
-  var webLink = "braytech.org/2/4611686018429000034/"+ fstats + "/legend";
-  $('#player-f-stats').text(fstats);
-    $("#player-web-Link").attr("href", webLink);
-        }
-
-      });
-      });
+       var fstats = data.Response.data.characters[0].characterBase.characterId;
+       var webLink = "braytech.org/2/4611686018429000034/"+ fstats + "/legend";
+       $('#player-f-stats').text(fstats);
+       $("#player-web-Link").attr("href", webLink);
+       }
+});
+});

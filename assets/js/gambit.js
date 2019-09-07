6bd696ddf926ac27fa15bff0db66ce75bae9ac3f
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-09-06 20:49:22 -0400

    Update gambit.js

--- assets/js/gambit.js
+++ assets/js/gambit.js
@@ -1,26 +1,26 @@
 $.ajax({
-    url: "https://www.bungie.net/Platform/Destiny/2/Account/4611686018429000034/",
-    headers: {
-      "X-API-Key": "47b810e692d64237911c2cbe0d433cfe"
-    }
-  }).done(function(json) {
+  url: "https://www.bungie.net/Platform/Destiny/2/Account/4611686018429000034/",
+  headers: {
+    "X-API-Key": "47b810e692d64237911c2cbe0d433cfe"
+  }
+}).done(function(json) {
 
-  });
-  $(function() {
-      $.ajax({
-        url: "https://www.bungie.net/Platform/Destiny/2/Account/4611686018429000034/",
-        headers: {
-          "X-API-Key": apiKey
-        },
-        success: function(data) {
-       var fstats = data.Response.data.characters[0].characterBase.characterId;
-       var webLink = "braytech.org/2/4611686018429000034/"+ fstats + "/legend";
-       $('#player-f-stats').text(fstats);
-       $("#player-web-Link").attr("href", webLink);
-       }
-        .html(
-       '<div class="j-col j-col-3 member-button"><a ' + 'webLink' + '</a></div>'
-         );  
-          
+});
+$(function() {
+    $.ajax({
+      url: "https://www.bungie.net/Platform/Destiny/2/Account/4611686018429000034/",
+      headers: {
+        "X-API-Key": apiKey
+      },
+      success: function(data) {
+     var fstats = data.Response.data.characters[0].characterBase.characterId;
+     var webLink = "braytech.org/2/4611686018429000034/"+ fstats + "/legend";
+     $('#player-f-stats').text(fstats);
+     $("#player-web-Link").attr("href", webLink)
+     
+      .html(
+     '<div class="j-col j-col-3 member-button"> '+ webLink +'</div>'
+       )
+      }
 });
 });

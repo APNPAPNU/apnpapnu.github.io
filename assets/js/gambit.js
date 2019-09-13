bd39991fb583569a9298d23d4cc292ad5bf1e4e8
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-09-13 14:19:47 -0400

    Update gambit.js

--- assets/js/gambit.js
+++ assets/js/gambit.js
@@ -36,110 +36,19 @@ $(function() {
 		success: function(data) {
 				  if (data.ErrorStatus === 'Success') {
 					  var
-			calcHours = function(activity) {
-			  // as long as the activity has at least 1 hour
-			  if (activity.indexOf('s') < 0) {
-				// if there's an 'm' leave off the minutes value
-				if (activity.indexOf('m') > -1) {
-				  var trim = activity.split('h');
-				  // return only hours
-				  return (Number(trim[0]));
-				} else {
-				  // if no 's' or 'm', convert days to hours + hours
-				  var hours = activity.match(/\d+/g);
-				  return ((Number(hours[0]) * 24) + Number(hours[1]));
-				}
-			  } else {
-				return 0;
-			  }
+			
+			  
+			  
 			},
 			// pvp stats
-			  stats = data.Response.allPvP.allTime,
-			  efficiency = stats.efficiency.basic.displayValue,
-			  kd = stats.killsDeathsRatio.basic.displayValue,
-			  kda = stats.killsDeathsAssists.basic.displayValue,
-			  kills = stats.kills.basic.displayValue,
-			  deaths = stats.deaths.basic.displayValue,
-			  assists = stats.assists.basic.displayValue,
-			  precisionKills = stats.precisionKills.basic.displayValue,
-			  combatRating = stats.combatRating.basic.displayValue,
-			  mostKills = stats.bestSingleGameKills.basic.displayValue,
-			  killSpree = stats.longestKillSpree.basic.displayValue,
-			  mostPrecision = stats.mostPrecisionKills.basic.displayValue,
-			  weapon = stats.weaponBestType.basic.displayValue,
-			  pvpClock = stats.secondsPlayed.basic.displayValue,
-			  pvpHours = calcHours(pvpClock),
-			// patrol stats
-			patrol = data.Response.patrol.allTime,
-		    patrolKills = patrol.kills.basic.displayValue,
-			patrolDeaths = patrol.deaths.basic.displayValue,
-		    patrolClock = patrol.secondsPlayed.basic.displayValue,
-			patrolHours = calcHours(patrolClock),
-		    patrolWeapon = patrol.weaponBestType.basic.displayValue,
-		    patrolHPE = patrol.heroicPublicEventsCompleted.basic.displayValue,
-			// raid stats
-			raid = data.Response.raid.allTime,
-			raidClears = raid.activitiesCleared.basic.displayValue,
-			raidKd = raid.killsDeathsRatio.basic.displayValue,
-			raidKills = raid.kills.basic.displayValue,
-			raidAvgKills = raid.kills.pga.displayValue,
-			raidBestKills = raid.bestSingleGameKills.basic.displayValue,
-			raidClock = raid.secondsPlayed.basic.displayValue,
-			raidHours = calcHours(raidClock),
-			// story stats
-			story = data.Response.story.allTime,
-			storyClock = story.secondsPlayed.basic.displayValue,
-			storyHours = calcHours(storyClock),
-			// strikes stats
-			strikes = data.Response.allStrikes.allTime,
-			strikesClears = strikes.activitiesCleared.basic.displayValue,
-			strikesKd = strikes.killsDeathsRatio.basic.displayValue,
-			strikesKills = strikes.kills.basic.displayValue,
-			strikesAvgKills = strikes.kills.pga.displayValue,
-			strikesBestKills = strikes.bestSingleGameKills.basic.displayValue,
-			strikesClock = strikes.secondsPlayed.basic.displayValue,
-			strikesHours = calcHours(strikesClock);
-  
-			totalHours = (patrolHours + pvpHours + raidHours + storyHours + strikesHours);
-			  console.log('Player stats:', data);
-			console.log('Player hours:\n Patrol:', patrolHours + 'h\n',
-			  'PvP:', pvpHours + 'h\n',
-			  'Raid:', raidHours + 'h\n',
-			  'Story:', storyHours + 'h\n',
-			  'Strikes:', strikesHours + 'h'
+			  stats = data.Response.mergedAllCharacters.results.allPve.allTime.weaponPrecisionKillsAutoRifle;
+			  
 			);
   
 			  // Populate stats
 			// pvp
-			  $('#player-clock').text(totalHours + 'h');
-			  $('#player-efficiency').text(efficiency);
-			  $('#player-kd').text(kd);
-			  $('#player-kda').text(kda);
-			  $('#player-kills').text(kills);
-			  $('#player-assists').text(assists);
-			  $('#player-precision-kills').text(precisionKills);
-			  $('#player-weapon').text(weapon);
-			  $('#player-kill-spree').text(killSpree);
-			  $('#player-most-kills').text(mostKills);
-			  $('#player-most-precision').text(mostPrecision);
-			// raid
-			$('#player-raid-clears').text(raidClears);
-			$('#player-raid-kd').text(raidKd);
-			$('#player-raid-kills').text(raidKills);
-			$('#player-raid-kills-pga').text(raidAvgKills);
-			$('#player-raid-best-kills').text(raidBestKills);
-			// strikes
-			$('#player-strike-clears').text(strikesClears);
-			$('#player-strike-kd').text(strikesKd);
-			$('#player-strike-kills').text(strikesKills);
-			$('#player-strike-kills-pga').text(strikesAvgKills);
-			$('#player-strike-best-kills').text(strikesBestKills);
-		//patrol
-		    $('#player-heroic-public-events').text(patrolHPE);
-			$('#player-patrol-kills').text(patrolKills);
-		    $('#player-patrol-deaths').text(patrolDeaths);
-			$('#player-patrol-best-weapon').text(patrolWeapon);
-		    $('#player-patrol-kills-pga').text(strikesAvgKills);
+			  $('#player-stats').text(stats);
+			 
 						} else {
 					  alert('Uh oh, failed to load player stats! Looks like Bungie\'s doing server maintenance or having problems. Please check back again soon!');
 					console.log(data);

91b07ab15b82da28c185226fb26580551da88dcb
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-09-18 18:08:11 -0400

    Update Carryabigrickpveweapons.js

--- assets/js/admin/Carryabigrickpveweapons.js
+++ assets/js/admin/Carryabigrickpveweapons.js
@@ -1,35 +1,9 @@
 $(function() {
 
-	var
-  
-	  bungieId = checkParams('bungieId'),
-	  destinyId = checkParams('destinyId'),
-	  joined = checkParams('joined'),
-	  checkName = function(name, list) {
-  
-		var m = false; // flag
-		console.log('Checking list for ' + name + '...');
-  
-		// loop through clan usernames and check for a match
-		$.each(list, function(i) {
-		  // make case insensitve
-		  if (name.toLowerCase() === list[i].toLowerCase()) {
-			console.log('Confirmed: ' + list[i]);
-			m = true;
-		  }
-		});
-  
-		if (m) {
-		  return true;
-		} else {
-		  return false;
-		}
-  
-      };
+
       $("#payBtn").click(function() {
         location.href = 'https://www.d2checklist.com/home'
       });
-	if (bungieId && destinyId && joined) {
 	  $.ajax({
 		url: "https://www.bungie.net/Platform/Destiny2/2/Account/4611686018428807356/Stats/?Groups=Weapons",
 		headers: {
@@ -94,4 +68,4 @@ $(function() {
 		  console.log('Error loading player stats:', data);
 		}
 	  });
-}});
+});

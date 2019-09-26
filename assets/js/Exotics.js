aeb2d69551c4065ee06861601e5ccb1774f2732b
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-09-25 20:18:22 -0400

    Create Exotics.js

---
+++ assets/js/Exotics.js
@@ -0,0 +1,61 @@
+    
+
+
+
+
+Telesto = Response.weapons[0].values.uniqueWeaponKills.basic.displayValue   
+TheHuckleberry = Response.weapons[1].values.uniqueWeaponKills.basic.displayValue   
+RatKing = Response.weapons[2].values.uniqueWeaponKills.basic.displayValue   
+Anarchy = Response.weapons[3].values.uniqueWeaponKills.basic.displayValue   
+SUROSRegime = Response.weapons[4].values.uniqueWeaponKills.basic.displayValue   
+Sunshot = Response.weapons[5].values.uniqueWeaponKills.basic.displayValue   
+D.A.R.C.I. = Response.weapons[6].values.uniqueWeaponKills.basic.displayValue   
+Borealis = Response.weapons[7].values.uniqueWeaponKills.basic.displayValue   
+Thunderlord = Response.weapons[8].values.uniqueWeaponKills.basic.displayValue   
+PolarisLance = Response.weapons[9].values.uniqueWeaponKills.basic.displayValue   
+Crimson = Response.weapons[10].values.uniqueWeaponKills.basic.displayValue   
+FightingLion = Response.weapons[11].values.uniqueWeaponKills.basic.displayValue   
+TractorCannon = Response.weapons[12].values.uniqueWeaponKills.basic.displayValue   
+LeMonarque = Response.weapons[13].values.uniqueWeaponKills.basic.displayValue   
+GravitonLance = Response.weapons[14].values.uniqueWeaponKills.basic.displayValue   
+VigilanceWing = Response.weapons[15].values.uniqueWeaponKills.basic.displayValue   
+BlackTalon = Response.weapons[16].values.uniqueWeaponKills.basic.displayValue   
+TheColony = Response.weapons[17].values.uniqueWeaponKills.basic.displayValue   
+SleeperSimulant = Response.weapons[18].values.uniqueWeaponKills.basic.displayValue   
+HardLight = Response.weapons[19].values.uniqueWeaponKills.basic.displayValue   
+Merciless = Response.weapons[20].values.uniqueWeaponKills.basic.displayValue   
+SkyburnersOath = Response.weapons[21].values.uniqueWeaponKills.basic.displayValue   
+PrometheusLens = Response.weapons[22].values.uniqueWeaponKills.basic.displayValue   
+Malfeasance = Response.weapons[23].values.uniqueWeaponKills.basic.displayValue   
+OutbreakPerfected = Response.weapons[24].values.uniqueWeaponKills.basic.displayValue   
+Jötunn = Response.weapons[25].values.uniqueWeaponKills.basic.displayValue   
+WishEnder = Response.weapons[26].values.uniqueWeaponKills.basic.displayValue   
+SweetBusiness = Response.weapons[27].values.uniqueWeaponKills.basic.displayValue   
+Coldheart = Response.weapons[28].values.uniqueWeaponKills.basic.displayValue   
+TheWardcliffCoil = Response.weapons[29].values.uniqueWeaponKills.basic.displayValue   
+LegendofAcrius = Response.weapons[30].values.uniqueWeaponKills.basic.displayValue   
+Wavesplitter = Response.weapons[31].values.uniqueWeaponKills.basic.displayValue   
+WhisperoftheWorm = Response.weapons[32].values.uniqueWeaponKills.basic.displayValue   
+Arbalest = Response.weapons[33].values.uniqueWeaponKills.basic.displayValue   
+TwoTailedFox = Response.weapons[34].values.uniqueWeaponKills.basic.displayValue   
+Sturm = Response.weapons[35].values.uniqueWeaponKills.basic.displayValue   
+Riskrunner = Response.weapons[36].values.uniqueWeaponKills.basic.displayValue   
+IzanagisBurden = Response.weapons[37].values.uniqueWeaponKills.basic.displayValue   
+TheChaperone = Response.weapons[38].values.uniqueWeaponKills.basic.displayValue   
+LordofWolves = Response.weapons[39].values.uniqueWeaponKills.basic.displayValue   
+TheProspector = Response.weapons[40].values.uniqueWeaponKills.basic.displayValue   
+Thorn = Response.weapons[41].values.uniqueWeaponKills.basic.displayValue   
+TheLastWord = Response.weapons[42].values.uniqueWeaponKills.basic.displayValue   
+Cerberus+ = Response.weapons[43].values.uniqueWeaponKills.basic.displayValue   
+WorldlineZero = Response.weapons[44].values.uniqueWeaponKills.basic.displayValue   
+TheQueenbreaker = Response.weapons[45].values.uniqueWeaponKills.basic.displayValue   
+BadJuju = Response.weapons[46].values.uniqueWeaponKills.basic.displayValue   
+Lumina = Response.weapons[47].values.uniqueWeaponKills.basic.displayValue   
+LegendofAcrius = Response.weapons[48].values.uniqueWeaponKills.basic.displayValue   
+TheJadeRabbit = Response.weapons[49].values.uniqueWeaponKills.basic.displayValue   
+AceofSpades = Response.weapons[50].values.uniqueWeaponKills.basic.displayValue   
+TrinityGhoul = Response.weapons[51].values.uniqueWeaponKills.basic.displayValue   
+Truth = Response.weapons[52].values.uniqueWeaponKills.basic.displayValue   
+MIDAMultiTool = Response.weapons[53].values.uniqueWeaponKills.basic.displayValue   
+    
+    

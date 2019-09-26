059488264757328babf13ffb69546b2d0ae0728e
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-09-26 18:09:12 -0400

    Update Exotics.js

--- assets/js/Exotics.js
+++ assets/js/Exotics.js
@@ -30,7 +30,7 @@
   
 	if (bungieId && destinyId && joined) {
 	  $.ajax({
-		url: "https://www.bungie.net/Platform/Destiny2/2/Account/'+destinyId+'/Character/0/Stats/UniqueWeapons/",
+		url: "https://www.bungie.net/Platform/Destiny2/2/Account/" + destinyId + "/Character/0/Stats/UniqueWeapons/",
         headers: {
             "X-API-Key": apiKey
           }, 

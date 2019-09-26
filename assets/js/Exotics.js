ec7fd173dd461bb853ce5c20b2774408d0417fe8
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-09-25 20:26:13 -0400

    Update Exotics.js

--- assets/js/Exotics.js
+++ assets/js/Exotics.js
@@ -1,4 +1,40 @@
-    
+  $(function() {
+
+	var
+  
+	  bungieId = checkParams('bungieId'),
+	  destinyId = checkParams('destinyId'),
+	  joined = checkParams('joined'),
+	  checkName = function(name, list) {
+  
+		var m = false; // flag
+		console.log('Checking list for ' + name + '...');
+  
+		// loop through clan usernames and check for a match
+		$.each(list, function(i) {
+		  // make case insensitve
+		  if (name.toLowerCase() === list[i].toLowerCase()) {
+			console.log('Confirmed: ' + list[i]);
+			m = true;
+		  }
+		});
+  
+		if (m) {
+		  return true;
+		} else {
+		  return false;
+		}
+  
+	  };
+  
+	if (bungieId && destinyId && joined) {
+	  $.ajax({
+		url: "https://www.bungie.net/Platform/Destiny2/2/Account/'+destinyId+'/Character/0/Stats/UniqueWeapons/,
+		headers: {
+		  "X-API-Key": apiKey
+		},
+		success: function(data) {
+				  if (data.ErrorStatus === 'Success') {  
 
 
 
@@ -108,3 +144,15 @@ MIDAMultiTool = Response.weapons[53].values.uniqueWeaponKills.basic.displayValue
   $('#player-FightingLion').text(FightingLion);
   $('#player-TractorCannon').text(TractorCannon);
   $('#player-VigilanceWing').text(VigilanceWing);
+} else {
+					  alert('Uh oh, failed to load player stats! Looks like Bungie\'s doing server maintenance or having problems. Please check back again soon!');
+					console.log(data);
+				  }
+  
+		},
+		error: function(data) {
+				  alert('Uh oh, failed to load player stats! Looks like Bungie\'s doing server maintenance or having problems. Please check back again soon!');
+		  console.log('Error loading player stats:', data);
+		}
+	  });
+}});

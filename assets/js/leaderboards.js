3f95e09c2b753966edfed4a2118f4e6e50405cca
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-10-06 21:20:46 -0400

    Update leaderboards.js

--- assets/js/leaderboards.js
+++ assets/js/leaderboards.js
@@ -18,7 +18,29 @@ thirdPlaceStat = data.Response.allPvP.lbSingleGameKills.entries[2].value.basic.d
 fourthPlaceName = data.Response.allPvP.lbSingleGameKills.entries[3].player.destinyUserInfo.displayName,
 fourthPlaceStat = data.Response.allPvP.lbSingleGameKills.entries[3].value.basic.displayValue,
 fifthPlaceName = data.Response.allPvP.lbSingleGameKills.entries[4].player.destinyUserInfo.displayName,
-fifthPlaceStat = data.Response.allPvP.lbSingleGameKills.entries[4].value.basic.displayValue;
+fifthPlaceStat = data.Response.allPvP.lbSingleGameKills.entries[4].value.basic.displayValue,
+                             allpvpTotalKillsFirstPlaceName = data.Response.allPvP.lbKills.entries[0].player.destinyUserInfo.displayName,
+                            allpvpTotalKillsFirstPlaceStat = data.Response.allPvP.lbKills.entries[0].value.basic.displayValue,
+                            allpvpTotalKillsSecondPlaceName = data.Response.allPvP.lbKills.entries[1].player.destinyUserInfo.displayName,
+                            allpvpTotalKillsSecondPlaceStat = data.Response.allPvP.lbKills.entries[1].value.basic.displayValue,
+                            allpvpTotalKillsThirdPlaceName = data.Response.allPvP.lbKills.entries[2].player.destinyUserInfo.displayName,
+                            allpvpTotalKillsThirdPlaceStat = data.Response.allPvP.lbKills.entries[2].value.basic.displayValue,
+                            allpvpTotalKillsFourthPlaceName = data.Response.allPvP.lbKills.entries[3].player.destinyUserInfo.displayName,
+                            allpvpTotalKillsFourthPlaceStat = data.Response.allPvP.lbKills.entries[3].value.basic.displayValue,
+                            allpvpTotalKillsFifthPlaceName = data.Response.allPvP.lbKills.entries[4].player.destinyUserInfo.displayName,
+                            allpvpTotalKillsFifthPlaceStat = data.Response.allPvP.lbKills.entries[4].value.basic.displayValue;
+                            
+                            $('#player-allpvp-Total-Kills-First-Place-Name').text(allpvpTotalKillsFirstPlaceName);
+                            $('#player-allpvp-Total-Kills-First-Place-Stat').text(allpvpTotalKillsFirstPlaceStat);
+                            $('#player-allpvp-Total-Kills-Second-Place-Name').text(allpvpTotalKillsSecondPlaceName);
+                            $('#player-allpvp-Total-Kills-Second-Place-Stat').text(allpvpTotalKillsSecondPlaceStat);
+                            $('#player-allpvp-Total-Kills-Third-Place-Name').text(allpvpTotalKillsThirdPlaceName);
+                            $('#player-allpvp-Total-Kills-Third-Place-Stat').text(allpvpTotalKillsThirdPlaceStat);
+                            $('#player-allpvp-Total-Kills-Fourth-Place-Name').text(allpvpTotalKillsFourthPlaceName);
+                            $('#player-allpvp-Total-Kills-Fourth-Place-Stat').text(allpvpTotalKillsFourthPlaceStat);
+                            $('#player-allpvp-Total-Kills-Fifth-Place-Name').text(allpvpTotalKillsFifthPlaceName);
+                            $('#player-allpvp-Total-Kills-Fifth-Place-Stat').text(allpvpTotalKillsFifthPlaceStat);
+  
 
 $('#player-first-Place-Name').text(firstPlaceName);
 $('#player-first-Place-Stat').text(firstPlaceStat);

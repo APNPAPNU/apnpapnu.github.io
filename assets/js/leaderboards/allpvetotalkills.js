607dd942c3e8593a01d7dcaa93329ae39b1c8c31
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-10-13 21:37:42 -0400

    Update allpvetotalkills.js

--- assets/js/leaderboards/allpvetotalkills.js
+++ assets/js/leaderboards/allpvetotalkills.js
@@ -1,7 +1,7 @@
 $(function() {
   
     $.ajax({
-       url: "https://www.bungie.net/Platform/Destiny2/Stats/Leaderboards/Clans/699392/?&modes=5&maxtop=100",
+       url: "https://www.bungie.net/Platform/Destiny2/Stats/Leaderboards/Clans/699392/?&modes=7&maxtop=100",
      headers: {
          "X-API-Key": "47b810e692d64237911c2cbe0d433cfe"
        }, 

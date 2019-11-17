5f24d869031fd5aec01b8787056760ac1e48c08b
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-11-17 17:35:37 -0500

    Update Exotics.js

--- assets/js/Exotics.js
+++ assets/js/Exotics.js
@@ -7,16 +7,24 @@ $(function() {
        
           if (bungieId && destinyId && joined) {
             $.ajax({
-               url: "https://www.bungie.net/Platform/Destiny2/2/Account/" + destinyId + "/Character/0/Stats/UniqueWeapons/",
+               url: "https://www.bungie.net/Platform/Destiny2/2/Account/4611686018429000034/Character/0/Stats/UniqueWeapons/",
              headers: {
                  "X-API-Key": apiKey
                }, 
                success: function(data) {
                            if (data.ErrorStatus === 'Success') {  
+     var names = {
+    347366834: 'AceofSpades',
+    460724140: 'TheJadeRabbit',
+    814876685: 'TrinityGhoul',
+    1345867570: 'SweetBusiness',
+}
+     // for example, the first weapon referenceId is 3973202132 (Thorn)
+var weaponId = response.weapons[0].referenceId
+var weaponId = response.weapons[1].referenceId
+var weaponName = names[weaponId]
+console.log(weaponName) // Thorn
      
-     
-test = data.Response.characterActivities.data.*.currentActivityModeType;
-     $(#test).text(test);
      } else {
                                 alert('Uh oh, failed to load player stats! Looks like Bungie\'s doing server maintenance or having problems. Please check back again soon!');
                               console.log(data);

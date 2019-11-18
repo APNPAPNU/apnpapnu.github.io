439bed65d6aa8651ffe201e6235c30e244873d11
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-11-18 15:07:37 -0500

    Update Exotics.js

--- assets/js/Exotics.js
+++ assets/js/Exotics.js
@@ -1,11 +1,6 @@
 $(function() {
      
-  var
-  bungieId = checkParams('bungieId'),
-  destinyId = checkParams('destinyId'),
-  joined = checkParams('joined');
-    
-       if (bungieId && destinyId && joined) {
+   {
          $.ajax({
             url: "https://www.bungie.net/Platform/Destiny2/2/Account/4611686018429000034/Character/0/Stats/UniqueWeapons/",
           headers: {

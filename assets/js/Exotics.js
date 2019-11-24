338df21654b59d309e07ff7fd73b3d1f96afa54b
APNPAPNU <jcpgraphicdesign@gmail.com>
2019-11-24 15:16:53 -0500

    Update Exotics.js

--- assets/js/Exotics.js
+++ assets/js/Exotics.js
@@ -7,11 +7,35 @@ $(function() {
              "X-API-Key": apiKey
            }, 
            success: function(data) {
-            let
-            NameT = jsonPath("$.Response..referenceId");
-                 $('#player-NameT').text(NameT);
-               
-               console.log(NameT);
+            var referenceText = `347366834 AceofSpades
+460724140   TheJadeRabbit
+814876685   TrinityGhoul
+1345867570  SweetBusiness`;
+
+// Just in case of an accidental whitespace character
+referenceText = referenceText.trim();
+
+// split this text into an array of lines (\r\n are newline characters)
+var items = reference.split(/[\r\n]/g);
+var names = {};
+// for each line in the array
+items.forEach(function(item) {
+    /*
+        So this is RegExp. Since the amount of space characters between
+        the name of the weapon and the ID varies, we use a regex which basically
+        says that "Just find as many spaces as possible, and
+        split the line into segments there
+    */
+    var data = item.split(/\s+/);
+    // the data array looks something like this ['347366834', 'AceofSpades']
+    var weaponId = data[0];
+    var weaponName = data[1];
+
+    // store the weapon name
+    names[weaponId] = weaponName
+});
+
+console.log(names);
    
            },
            

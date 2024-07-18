cfd39ea20a2dfb3874b0d4849089a87938e35179
apnu <jcpgraphicdesign@gmail.com>
2024-07-17 21:29:04 -0400

    Update roster.js

--- TEST/roster.js
+++ TEST/roster.js
@@ -1,27 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const axios = require('axios');
     const apiKey = "47b810e692d64237911c2cbe0d433cfe";
     const groupId = "699392";
     const url = `https://www.bungie.net/Platform/GroupV2/${groupId}/Members/`;
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.setRequestHeader('X-API-Key', apiKey);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                const data = JSON.parse(xhr.responseText);
-                console.log('API Response:', data);
-                if (data.ErrorStatus === 'Success') {
-                    displayRoster(data.Response.results);
-                } else {
-                    console.error('Error fetching roster:', data.Message);
-                    alert('Error fetching roster: ' + data.Message);
-                }
-            } else {
-                console.error('Request failed. Status:', xhr.status);
-                alert('Request failed. Status: ' + xhr.status);
-            }
+    axios.get(url, {
+        headers: {
+            'X-API-Key': apiKey
         }
-    };
-    xhr.send();
+    })
+    .then(response => {
+        console.log('API Response:', response.data);
+        if (response.data.ErrorStatus === 'Success') {
+            displayRoster(response.data.Response.results);
+        } else {
+            console.error('Error fetching roster:', response.data.Message);
+            alert('Error fetching roster: ' + response.data.Message);
+        }
+    })
+    .catch(error => {
+        console.error('Axios Error:', error);
+        alert('Axios Error: ' + error.message);
+    });
 });
